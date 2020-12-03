2a898c037a98cac5e15e30b3f432df1562bb5182
Mosaddik Ali Maruf <malinjr07@gmail.com>
2020-12-03 19:02:06 +0600

    business

---
+++ filterable-gallery-using-filterizr-js/dist/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+			$('.filterizr-filter li').click(function() {
+			  $('.filterizr-filter li').removeClass('filtr-active');
+			  $(this).addClass('filtr-active');
+			});
+      $('.filterizr-sorting li').click(function() {
+			  $('.filterizr-sorting li').removeClass('filtr-active');
+			  $(this).addClass('filtr-active');
+			});
+  
+			var filterizd = $('.filtr-container').filterizr();
+
+			filterizd.filterizr('sort', 'title', 'asc');
+
+		});
